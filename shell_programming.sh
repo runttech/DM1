062394026832073b26f5bb93afe0cda180758164
kreaux77@gmail.com <salvistimosenko@inbox.lv>
2017-09-27 11:47:00 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+#4. piemers
+echo "Tiek pildits skripts: "$0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: "$#
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 1):"$*
+echo "Skriptam nodoti argumenti (attelošana/grupēšana 2):"$@
+#echo $?
+echo "Skriptam piešķirtais procesa numurs"$$
+#echo $!
 
-NAME="Vards Uzvards"
+#3. piemers
+#NAME="Vards Uzvards"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
-
+#2. piemers
+#NAME="Vards Uzvards"
 #readonly NAME
 #echo $NAME
 #NAME="Uzvards Vards"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
+#1. piemers
+#NAME="Vards Uzvards"
+#echo $NAME
